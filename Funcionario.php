2a65a58b08cbe41f2e9d389420c9cd9fb03bf1ab
djesusguimaraes <jesus.daniel@uft.edu.br>
2020-12-18 06:15:58 -0300

    js é máscara pros campos fiz esse testes pra facilitar a rodagem aqui

--- Funcionario.php
+++ Funcionario.php
@@ -27,3 +27,30 @@ require 'connForDB.php';
     header('Location: index.php');
 
 ?>
+<html lang="pt-br">
+<header>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="normalize_pure.css">
+</header>
+    <div id="cadastroFuncionario">
+    <form action="" method='POST'>
+        <div>
+        <label for="nome">Nome Completo: </label>
+        <input type="text" id="name">
+        <script>
+            function rand() {
+                document.getElementById("gera").innerHTML = Math.floor(Math.random() * 65536);    
+            }
+        </script>
+        <h4>Número de Identificação:</h4>
+        <label for="idFuncionario" id="gera" onclick="rand()">Gerar</label>
+        <label for="cpf">CPF:</label>
+        <input type="text" name="cpf" \ pattern="\d{3}\.\d{3}\.\d{3}-\d{2}" \ title="Digite um CPF no formato: xxx.xxx.xxx-xx">
+        <label for="idFuncao">Cargo:</label>
+        <input type="text">
+        </div>
+    </form>
+
+    </div>
+
+</html>
--- index.php
+++ index.php
@@ -4,3 +4,15 @@ header('Location: cadastroCarro');
 header('Location: cadastroCliente');
 header('Location: Funcionario');
 header('Location: Servico');
+
+?>
+<html lang="pt-br">
+<head>
+    <meta charset="UFT-8">
+    
+    <title>Home / Javalato</title>
+</head>
+<body>
+
+</body>
+</html>
